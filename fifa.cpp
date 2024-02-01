c8145e8936995762159dacdc26019ab61ecc2de4
snipchu <pipandw@gmail.com>
2024-02-01 10:45:25 -0600

    2 solutions finish

---
+++ fifa.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main() {
+  int n; std::cin >> n;
+  int k; std::cin >> k;
+  std::cout << (n/k)+2022 << "\n";
+}
---
+++ tolvunarfraedingartelja.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+
+  int inputNum{ };
+  std::cin >> inputNum;
+  std::cout << inputNum - 1;
+  return 0;
+}

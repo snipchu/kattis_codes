dad77a3be8fcb522d7e57f61c5ab72b90446ce7f
nicholas bunnyguy <pipandw@gmail.com>
2024-02-26 13:37:05 -0800

    more codes:D

---
+++ flatbokuveisla.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  int n{};
+  std::cin >> n;
+
+  int m{};
+  std::cin >> m;
+
+  std::cout << n%m << "\n";
+
+}
---
+++ ovissa.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  std::string u{};
+  std::cin >> u;
+  std::cout << u.length() << "\n";
+}
